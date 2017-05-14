786f65c589e2790c0a0eb24f05e9e4088f773644
JavierIF <javier.ifer@gmail.com>
2017-05-14 20:13:47 +0200

    Añadido la llamada al servicio Luis Cambiado de WebClient a HttpClient pues que httpclient provee una mayor funcionalidad.

--- BotTiempo/BotTiempo/Services/LuisServicio.cs
+++ BotTiempo/BotTiempo/Services/LuisServicio.cs
@@ -1,14 +1,36 @@
 ﻿using BotTiempo.Model;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
-using System.Web;
 
 namespace BotTiempo.Services
 {
     public class LuisServicio
     {
+        public static async Task<LuisResponse> ParseUserConversation(string texto)
+        {
 
+            var luisResponse = new LuisResponse();
+
+            using (var client = new HttpClient())
+            {
+                string escapedString = Uri.EscapeDataString(texto);
+                string uri = $"https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/ecbc0b1f-e1bd-4a2a-86dc-b67e99165c83?subscription-key=ae40a0cafdba4a62959aae4b6d4d3a3b&verbose=true&timezoneOffset=0&q={escapedString}";
+
+                var msg = await client.GetAsync(uri);
+                if(msg.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await msg.Content.ReadAsStringAsync();
+                    var data = JsonConvert.DeserializeObject<LuisResponse>(jsonResponse);
+                    return data;
+                }
+               
+            }
+            return luisResponse;
+        }
     }
 }
--- BotTiempo/BotTiempo/Services/TiempoServicio.cs
+++ BotTiempo/BotTiempo/Services/TiempoServicio.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web;
 
@@ -13,30 +14,24 @@ namespace BotTiempo.Services
     {
         public static async Task<TiempoItem> GetTiempo(string lugar)
         {
+            string escapedString = Uri.EscapeDataString(lugar);
             //El $ delante es una forma de hacer un string.format rápido para asignar el parametro.
-            string uri = $"http://api.apixu.com/v1/current.json?key=a27ce9eeba084165a8a104719171405&q={lugar}";
+            string uri = $"http://api.apixu.com/v1/current.json?key=a27ce9eeba084165a8a104719171405&q={escapedString}";
             var tiempoItem = new TiempoItem();
             //Using sirve para crear un bloque de ejecución donde liberará los recursos al salir.
-            using (var client = new WebClient())
+            using (var client = new HttpClient())
             {
-                try
+                
+                var msg = await client.GetAsync(uri);
+                if (msg.IsSuccessStatusCode)
                 {
-                    //Descargamos el contenido json sin formateo
-                    var rawData = await client.DownloadStringTaskAsync(new Uri(uri));
-                    tiempoItem = JsonConvert.DeserializeObject<TiempoItem>(rawData);
-                    //Añadimos una comprobación de que se ha encontrado correctamente el lugar.
-                    if (tiempoItem.location.Equals(null))
-                    {
-                        tiempoItem.Status = "ERROR";
-                    }
-                    else
-                    {
-                        tiempoItem.Status = "OK";
-                    }
-                } catch(Exception e)
+                    var jsonResponse = await msg.Content.ReadAsStringAsync();
+                    tiempoItem = JsonConvert.DeserializeObject<TiempoItem>(jsonResponse);
+                    tiempoItem.Status = "OK";
+                } else
                 {
                     tiempoItem.Status = "ERROR";
-                }
+                }             
 
             }
             return tiempoItem;
