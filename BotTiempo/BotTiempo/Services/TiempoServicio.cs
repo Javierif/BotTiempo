47fd538f16779582fc51bd104e08fbd954865d12
JavierIF <javier.ifer@gmail.com>
2017-05-14 13:01:01 +0200

    Añadimos el servicio para obtener el tiempo y el modelo.

---
+++ BotTiempo/BotTiempo/Services/TiempoServicio.cs
@@ -0,0 +1,28 @@
+﻿using BotTiempo.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BotTiempo.Services
+{
+    public class TiempoServicio
+    {
+        public static async Task<TiempoItem> GetTiempo(string lugar)
+        {
+            //El $ delante es una forma de hacer un string.format rápido para asignar el parametro.
+            string uri = $"http://api.apixu.com/v1/current.json?key=a27ce9eeba084165a8a104719171405&q={lugar}";
+            //Using sirve para crear un bloque de ejecución donde liberará los recursos al salir.
+            using (var client = new WebClient())
+            {
+                //Descargamos el contenido json sin formateo
+                var rawData = await client.DownloadStringTaskAsync(new Uri(uri));
+                var tiempoItem = JsonConvert.DeserializeObject<TiempoItem>(rawData);
+            }
+                
+         }
+    }
+}
